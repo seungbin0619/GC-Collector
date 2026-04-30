266de93674992f16deae20ff25da09e80f89fdf5
seungbin0619 <seungbin0619@naver.com>
2026-04-30 15:00:37 +0900

    feat: Gen lua translators

--- GC-Collector/Assets/01_Shared/Entity/Script/EntityData.cs
+++ GC-Collector/Assets/01_Shared/Entity/Script/EntityData.cs
@@ -1,6 +1,8 @@
 using Unity.Mathematics;
+using XLua;
 
 namespace Gcc.Shared {
+    [GCOptimize]
     public struct EntityData {
         public int id;
         public float2 position;
--- GC-Collector/Assets/ThirdParty/XLua/LuaGenConfig.cs
+++ GC-Collector/Assets/ThirdParty/XLua/LuaGenConfig.cs
@@ -1,46 +1,46 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using XLua;
-using Gcc.Shared;
-using Unity.Mathematics;
+// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using XLua;
+// using Gcc.Shared;
+// using Unity.Mathematics;
 
-public static class LuaGenConfig {
-    [LuaCallCSharp]
-    public static List<Type> LuaCallCSharp = new() {
-        // unity types
-        typeof(GameObject),
-        typeof(Transform),
-        typeof(Time),
-        typeof(Debug),
-        typeof(Vector3),
-        typeof(Vector2),
-        typeof(Quaternion),
-        typeof(Mathf),
+// public static class LuaGenConfig {
+//     [LuaCallCSharp]
+//     public static List<Type> LuaCallCSharp = new() {
+//         // unity types
+//         typeof(GameObject),
+//         typeof(Transform),
+//         typeof(Time),
+//         typeof(Debug),
+//         typeof(Vector3),
+//         typeof(Vector2),
+//         typeof(Quaternion),
+//         typeof(Mathf),
 
-        // custom types
-        typeof(EntityData),
-        typeof(EntityData[]),
+//         // custom types
+//         typeof(EntityData),
+//         typeof(EntityData[]),
         
-        // mathematics
-        typeof(float2),
-        typeof(float3)
-    };
+//         // mathematics
+//         typeof(float2),
+//         typeof(float3)
+//     };
 
-    [CSharpCallLua]
-    public static List<Type> CSharpCallLua = new() {
-        typeof(Action),
-        typeof(Action<string>),
-        typeof(Action<double>),
-        typeof(Action<EntityData[]>),
-        typeof(Func<double, double, double>),
-        typeof(System.Collections.IEnumerator)
-    };
+//     [CSharpCallLua]
+//     public static List<Type> CSharpCallLua = new() {
+//         typeof(Action),
+//         typeof(Action<string>),
+//         typeof(Action<double>),
+//         typeof(Action<EntityData[]>),
+//         typeof(Func<double, double, double>),
+//         typeof(System.Collections.IEnumerator)
+//     };
 
-    [GCOptimize]
-    public static List<Type> GCOptimize = new() {
-        typeof(EntityData),
-        typeof(float2),
-        typeof(float3)
-    };
-}
+//     [GCOptimize]
+//     public static List<Type> GCOptimize = new() {
+//         typeof(EntityData),
+//         typeof(float2),
+//         typeof(float3)
+//     };
+// }
